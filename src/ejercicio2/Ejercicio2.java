db2421852b82353e7b0b8382637da66b6c145e65
ESTUDIANTE <ESTUDIANTE@DESKTOP-KTO51U0>
2017-08-09 15:26:51 -0500

    karel_ejercicio1

---
+++ src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio2;
+import java.util.*;
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
